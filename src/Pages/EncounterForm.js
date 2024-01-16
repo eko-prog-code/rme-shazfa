7d1982729a40d0db27bac7a19fc952723f812ca1
Abdur Rahim <abdoerrahiem@gmail.com>
2024-01-16 16:07:51 +0700

    fix cors

--- src/Pages/EncounterForm.js
+++ src/Pages/EncounterForm.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import './EncounterForm.css';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import './EncounterForm.css'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const EncounterForm = () => {
-  const navigate = useNavigate();
-  const [accessToken, setAccessToken] = useState(null);
+  const navigate = useNavigate()
+  const [accessToken, setAccessToken] = useState(null)
 
   useEffect(() => {
-    fetchTokenFromFirebase();
-  }, []);
+    fetchTokenFromFirebase()
+  }, [])
 
   const fetchTokenFromFirebase = async () => {
     try {
-      const firebaseTokenUrl = 'https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json';
-      const response = await axios.get(firebaseTokenUrl);
-      const tokenFromFirebase = response.data.token;
-  
+      const firebaseTokenUrl =
+        'https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json'
+      const response = await axios.get(firebaseTokenUrl)
+      const tokenFromFirebase = response.data.token
+
       if (tokenFromFirebase) {
-        console.log('Token dari Firebase:', tokenFromFirebase);
-        return tokenFromFirebase;
+        console.log('Token dari Firebase:', tokenFromFirebase)
+        return tokenFromFirebase
       } else {
-        console.error('Token tidak ditemukan dari Firebase.');
-        return null;
+        console.error('Token tidak ditemukan dari Firebase.')
+        return null
       }
     } catch (error) {
-      console.error('Error fetching access token from Firebase:', error);
-      return null;
+      console.error('Error fetching access token from Firebase:', error)
+      return null
     }
-  };
-  
-  
+  }
 
   const [formData, setFormData] = useState(() => {
     // Initialize formData dynamically based on input fields
     const initialFormData = {
-      identifierSystem: 'http://sys-ids.kemkes.go.id/encounter/dfd92855-8cec-4a10-be94-8edd8a097344',
+      identifierSystem:
+        'http://sys-ids.kemkes.go.id/encounter/dfd92855-8cec-4a10-be94-8edd8a097344',
       identifierValue: '',
       subjectReference: 'Patient/100000030005',
       subjectDisplay: 'Jiwa tangguh',
@@ -43,100 +43,190 @@ const EncounterForm = () => {
       participantDisplay: 'Dokter Bronsig',
       periodStart: '2022-06-14T07:00',
       statusHistoryStart: '2022-06-14T07:00',
-      serviceProviderReference: 'Organization/dfd92855-8cec-4a10-be94-8edd8a097344',
+      serviceProviderReference:
+        'Organization/dfd92855-8cec-4a10-be94-8edd8a097344',
       locationReference: 'Location/b017aa54-f1df-4ec2-9d84-8823815d7228',
-      locationDisplay: 'Ruang 1A, Poliklinik Bedah Rawat Jalan Terpadu, Lantai 2, Gedung G',
+      locationDisplay:
+        'Ruang 1A, Poliklinik Bedah Rawat Jalan Terpadu, Lantai 2, Gedung G',
       // ... (add other form fields)
-    };
-    return initialFormData;
-  });
+    }
+    return initialFormData
+  })
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
-  };
+    }))
+  }
 
   const resetForm = () => {
     setFormData(() => ({
       // Reset form data here
       // ...
-    }));
-  };
+    }))
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-  
-    try {
-      const tokenFromFirebase = await fetchTokenFromFirebase();
-  
-      if (!tokenFromFirebase) {
-        console.error('Access token is not available.');
-        return;
-      }
-  
-      console.log('Access Token:', tokenFromFirebase);
-  
-      const response = await fetch('http://localhost:5000/fhir-r4/v1/Encounter', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${tokenFromFirebase}`,
+    e.preventDefault()
+    fetchTokenFromFirebase()
+      .then((res) => saveData(res))
+      .catch((err) => console.log(err))
+  }
+
+  const saveData = async (tokenFromFirebase) => {
+    const data = {
+      resourceType: 'Encounter',
+      status: 'arrived',
+      class: {
+        system: 'http://terminology.hl7.org/CodeSystem/v3-ActCode',
+        code: 'AMB',
+        display: 'ambulatory',
+      },
+      subject: {
+        reference: formData.subjectReference,
+        display: formData.subjectDisplay,
+      },
+      participant: [
+        {
+          type: [
+            {
+              coding: [
+                {
+                  system:
+                    'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
+                  code: 'ATND',
+                  display: 'attender',
+                },
+              ],
+            },
+          ],
+          individual: {
+            reference: formData.participantReference,
+            display: formData.participantDisplay,
+          },
         },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        console.log('Data berhasil dikirim:', response);
-        resetForm(); // Reset the form after successful submission
-      } else {
-        console.error('Gagal mengirim data:', response);
-      }
-    } catch (error) {
-      console.error('Gagal mengirim data:', error);
+      ],
+      period: {
+        start: formData.periodStart + ':00+07:00',
+      },
+      location: [
+        {
+          location: {
+            reference: formData.locationReference,
+            display: formData.locationDisplay,
+          },
+        },
+      ],
+      statusHistory: [
+        {
+          status: 'arrived',
+          period: {
+            start: formData.statusHistoryStart + ':00+07:00',
+          },
+        },
+      ],
+      serviceProvider: {
+        reference: formData.serviceProviderReference,
+      },
+      identifier: [
+        {
+          system: formData.identifierSystem,
+          value: formData.identifierValue,
+        },
+      ],
     }
-  };
-  
+
+    setLoading(true)
+    axios
+      .post('/Encounter', data, {
+        headers: {
+          Authorization: `Bearer ${tokenFromFirebase}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        console.log('data: ', res)
+        resetForm()
+      })
+      .catch((err) => console.error('Gagal mengirim data:', err.response))
+      .finally(() => setLoading(false))
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
           Identifier System:
-          <input type="text" name="identifierSystem" value={formData.identifierSystem} onChange={handleChange} readOnly />
+          <input
+            type='text'
+            name='identifierSystem'
+            value={formData.identifierSystem}
+            onChange={handleChange}
+            readOnly
+          />
         </label>
         <label>
           Identifier Value:
-          <input type="text" name="identifierValue" value={formData.identifierValue} onChange={handleChange} />
+          <input
+            type='text'
+            name='identifierValue'
+            value={formData.identifierValue}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Subject Reference:
-          <input type="text" name="subjectReference" value={formData.subjectReference} onChange={handleChange} />
+          <input
+            type='text'
+            name='subjectReference'
+            value={formData.subjectReference}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Subject Display:
-          <input type="text" name="subjectDisplay" value={formData.subjectDisplay} onChange={handleChange} />
+          <input
+            type='text'
+            name='subjectDisplay'
+            value={formData.subjectDisplay}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Participant Reference:
-          <input type="text" name="participantReference" value={formData.participantReference} onChange={handleChange} />
+          <input
+            type='text'
+            name='participantReference'
+            value={formData.participantReference}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Participant Display:
-          <input type="text" name="participantDisplay" value={formData.participantDisplay} onChange={handleChange} />
+          <input
+            type='text'
+            name='participantDisplay'
+            value={formData.participantDisplay}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Period Start:
-          <input type="datetime-local" name="periodStart" value={formData.periodStart} onChange={handleChange} />
+          <input
+            type='datetime-local'
+            name='periodStart'
+            value={formData.periodStart}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Status History Start:
           <input
-            type="datetime-local"
-            name="statusHistoryStart"
+            type='datetime-local'
+            name='statusHistoryStart'
             value={formData.statusHistoryStart}
             onChange={handleChange}
           />
@@ -145,8 +235,8 @@ const EncounterForm = () => {
         <label>
           Service Provider Reference:
           <input
-            type="text"
-            name="serviceProviderReference"
+            type='text'
+            name='serviceProviderReference'
             value={formData.serviceProviderReference}
             onChange={handleChange}
           />
@@ -154,19 +244,32 @@ const EncounterForm = () => {
 
         <label>
           Location Reference:
-          <input type="text" name="locationReference" value={formData.locationReference} onChange={handleChange} />
+          <input
+            type='text'
+            name='locationReference'
+            value={formData.locationReference}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Location Display:
-          <input type="text" name="locationDisplay" value={formData.locationDisplay} onChange={handleChange} />
+          <input
+            type='text'
+            name='locationDisplay'
+            value={formData.locationDisplay}
+            onChange={handleChange}
+          />
         </label>
         {/* ... (Input fields lainnya) */}
-        <button type="submit" style={{ backgroundColor: '#2196F3', color: '#ffffff' }}>
-          Kirim Data
+        <button
+          type='submit'
+          style={{ backgroundColor: '#2196F3', color: '#ffffff' }}
+        >
+          {loading ? 'Loading...' : 'Kirim Data'}
         </button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default EncounterForm;
+export default EncounterForm
--- src/Pages/auth.js
+++ src/Pages/auth.js
@@ -1,48 +1,49 @@
-import axios from 'axios';
+import axios from 'axios'
 
 export const getToken = async () => {
   try {
-    const clientId = "TwLnADX6F27jCpvfGIodNeNsPOAxWz8KypXhkTkyhQXGYrbJ";
+    const clientId = 'TwLnADX6F27jCpvfGIodNeNsPOAxWz8KypXhkTkyhQXGYrbJ'
     const clientSecret =
-      "LLYCw4UHEQ8skMPKAehK1N0ojKrb0GI84RjwdclEXZ0zbICPdxT7BZW65BUQEP6W";
+      'LLYCw4UHEQ8skMPKAehK1N0ojKrb0GI84RjwdclEXZ0zbICPdxT7BZW65BUQEP6W'
 
-    const formData = new URLSearchParams();
-    formData.append("client_id", clientId);
-    formData.append("client_secret", clientSecret);
+    const formData = new URLSearchParams()
+    formData.append('client_id', clientId)
+    formData.append('client_secret', clientSecret)
 
     const response = await axios.post(
-      "https://api-satusehat-dev.dto.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials",
+      'https://api-satusehat-dev.dto.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials',
       formData,
       {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
       }
-    );
+    )
 
-    const accessToken = response.data.access_token;
+    const accessToken = response.data.access_token
 
     // Store the token in Firebase Realtime Database
-    await storeTokenInFirebase(accessToken);
+    await storeTokenInFirebase(accessToken)
 
-    return accessToken;
+    return accessToken
   } catch (error) {
-    console.error("Error fetching access token:", error.message);
-    throw error; // Propagate the error to the calling code
+    console.error('Error fetching access token:', error.message)
+    throw error // Propagate the error to the calling code
   }
-};
+}
 
 const storeTokenInFirebase = async (accessToken) => {
   try {
     // Firebase Realtime Database URL
-    const firebaseUrl = "https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json";
+    const firebaseUrl =
+      'https://rme-shazfa-mounira-default-rtdb.firebaseio.com/token.json'
 
     // Use Axios to store the token in Firebase
-    await axios.put(firebaseUrl, { token: accessToken });
+    await axios.put(firebaseUrl, { token: accessToken })
 
-    console.log("Token successfully stored in Firebase!");
+    console.log('Token successfully stored in Firebase!')
   } catch (error) {
-    console.error("Error storing token in Firebase:", error.message);
-    throw error; // Propagate the error to the calling code
+    console.error('Error storing token in Firebase:', error.message)
+    throw error // Propagate the error to the calling code
   }
-};
+}
