7b23c3265a874905ebb8c1e6f954743bfadba961
Abdur Rahim <abdoerrahiem@gmail.com>
2024-01-17 21:51:46 +0700

    check dev

--- src/Pages/EncounterForm.js
+++ src/Pages/EncounterForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './EncounterForm.css'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { API_SATU_SEHAT, IS_DEV } from '../utils/constant'
 
 const EncounterForm = () => {
   const navigate = useNavigate()
@@ -141,7 +142,7 @@ const EncounterForm = () => {
 
     setLoading(true)
     axios
-      .post('/Encounter', data, {
+      .post(IS_DEV ? '/Encounter' : `${API_SATU_SEHAT}/Encounter`, data, {
         headers: {
           Authorization: `Bearer ${tokenFromFirebase}`,
           'Content-Type': 'application/json',
